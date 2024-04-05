f4f4d2acc5677c621e193f565b43e4256a986a30
siyu <csyslow@gmail.com>
2024-04-05 17:42:24 -0400

    project prep completed

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+       <div style={{width:'750rem', height:'200px',backgroundColor:'green'}}></div>
+    );
+};
+
+export default App;
